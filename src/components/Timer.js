dba354051863a717758ff4e5bac033a3bf50346d
Denys Mitlenko <softonka@gmail.com>
2024-01-14 22:41:50 +0200

    initial commit

---
+++ src/components/Timer.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+
+const Timer = ({ dispatch, secondsRemaining }) => {
+  const minutes = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      dispatch({ type: "tick" });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [dispatch]);
+
+  return (
+    <div className="timer">
+      {minutes < 10 && "0"}
+      {minutes}:{seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
+};
+
+export default Timer;
